7c7df22bafd031f19c81ae96413a78609563610d
VINX CODER <m.disanthraj2009@gmail.com>
2025-08-30 12:49:23 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~UmNlAYQK#Q8fvGNihoovnjOS6P0Vrp0Unh1v1KYELj-hiCk0Ape0",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
